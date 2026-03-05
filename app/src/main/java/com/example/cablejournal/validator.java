09b487ceab99034544b00d87fdc739a4d083c898
raystleen <rayst.rm@gmail.com>
2026-03-05 12:47:11 +0300

    test2

--- app/src/main/java/com/example/cablejournal/validator.java
+++ app/src/main/java/com/example/cablejournal/validator.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 
 public class validator {
 
-    int a = 123;
+    int a = 1234;
 
     public static <T extends TextView> boolean isEmpty(Context context, T elem)
     {
